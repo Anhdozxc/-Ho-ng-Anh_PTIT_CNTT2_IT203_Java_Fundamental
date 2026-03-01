775a06faede7426772910a5befc7fdf2669b23c5
-Ho-ng-Anh_KSENG24C_HTML_Session04 <dohoanganh.russia@gmail.com>
2026-03-01 23:02:25 +0700

    Upload_Session14

---
+++ src/session14/B1.java
@@ -0,0 +1,11 @@
+package session14;
+import java.util.*;
+
+public class B1 {
+    public static void main(String[] args) {
+        Set<String> set = new LinkedHashSet<>(List.of("Nguyễn Văn A – Yên Bái", "Trần Thị B – Thái Bình", "Nguyễn Văn A – Yên Bái", "Lê Văn C – Hưng Yên"));
+        for (String s : set) {
+            System.out.println(s);
+        }
+    }
+}
---
+++ src/session14/B2.java
@@ -0,0 +1,24 @@
+package session14;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class B2 {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("T01", "Paracetamol");
+        map.put("T02", "Ibuprofen");
+        map.put("T03", "Aspirin");
+        map.put("T04", "Amoxicillin");
+        map.put("T05", "Vitamin C");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Mời bạn nhập vào mã thuốc: ");
+        String tmp = sc.nextLine();
+        if (map.containsKey(tmp)) {
+            System.out.println("Tên thuốc: "+ map.get(tmp));
+        }
+        else System.out.println("Thuốc không tồn tại!!");
+    }
+}
